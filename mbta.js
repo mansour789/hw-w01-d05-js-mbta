d7f9397e57e1cda8696b0895afbbe02acae22c02
mansour789 <m0509111136@gmail.com>
2019-06-22 17:06:53 +0300

    Adding the bonus

--- mbta.js
+++ mbta.js
@@ -17,10 +17,12 @@ const stopsBetweenStations = function(startLine, startStation, endLine, endStati
         let toParkStreet = lines[startLine].indexOf("Park Street");
         let fromParkStreet = lines[endLine].indexOf("Park Street");
         let endingPoint = lines[endLine].indexOf(endStation);
-
         let stopsToChange = 0;
         let stopsAfterChange = 0;
         let stops = 0;
+        let stations = [];
+        let stationsBeforChange = [];
+        let stationsAfterChange = [];
 
 
         if (startPoint === -1 ){
@@ -30,39 +32,86 @@ const stopsBetweenStations = function(startLine, startStation, endLine, endStati
             return ` ${endStation} is NOT on  ${endLine} Line. Please check your arriving station!`;
         }
         if (startStation === endStation){
-            return `Hey dude !!  ${startStation} && ${endStation} are the Same!!!.`;
+            return `Hey dude !! ${startStation} in ${startLine} Line is the same as ${endStation} in ${endLine} Line!!!.`;
         }
         
 
         if (startLine === endLine){
                 //If the rider travils in the same line
+                console.log(`Rider boards the train a ${startLine} Line and ${startStation}.`)
+                if ( startPoint > endingPoint){
+                    stops = startPoint - endingPoint;
+
+                    for (let k = startPoint; k > endingPoint - 1; k--){
+                         stations.push(lines[startLine][k]);   
+                    };
+                     for (let j = 1; j < stations.length - 1; j++){
+                     console.log(`Rider arrives at ${startLine} Line and ${stations[j]}.`)
+                     };
+                }else{
+                    stops = endingPoint - startPoint;
+                    for (let i = startPoint; i < endingPoint + 1; i++){
+                        stations.push(lines[startLine][i]); 
+                    };
+                     for (let j = 1; j < stations.length - 1; j++){
+                    console.log(`Rider arrives at ${startLine} Line and ${stations[j]}.`)
+                     };
                     
-                    if ( startPoint > endingPoint){
-                        stops = startPoint - endingPoint;
-                    }else{
-                        stops = endingPoint - startPoint;
-                    }
-                    
-         }else {
+                };
+                console.log(`Rider exits the train at ${endLine} Line and ${endStation}.`);
+     }else {
                     //If the rider travils in different lines
+                    console.log(`Rider boards the train a ${startLine} Line and ${startStation}.`);
+                
+                if ( startPoint > toParkStreet){
+                    // From the Stat to Park Street
+                    stopsToChange = startPoint - toParkStreet;
+                    for (let k = startPoint; k > toParkStreet - 1; k--){
+                        stationsBeforChange.push(lines[startLine][k]);   
+                   };
+                    for (let j = 1; j < stationsBeforChange.length - 1; j++){
+                    console.log(`Rider arrives at ${startLine} Line and ${stationsBeforChange[j]}.`)
+                    };
                     
-                    if ( startPoint > toParkStreet){
-                        // From the Stat to Park Street
-                        stopsToChange = startPoint - toParkStreet;
-                    }else{
-                        stopsToChange = toParkStreet - startPoint;
-                    }
+                }else{
+                    stopsToChange = toParkStreet - startPoint;
+                    for (let i = startPoint; i < toParkStreet + 1; i++){
+                        stationsBeforChange.push(lines[startLine][i]); 
+                    };
+                     for (let j = 1; j < stationsBeforChange.length - 1; j++){
+                    console.log(`Rider arrives at ${startLine} Line and ${stationsBeforChange[j]}.`)
+                     };
                     
-                    if ( fromParkStreet > endingPoint){
-                        // From Park Street to the End
-                        stopsAfterChange = fromParkStreet - endingPoint;
-                    }else{
-                        stopsAfterChange = endingPoint - fromParkStreet;
-                    }
-                    stops = stopsToChange + stopsAfterChange;
+                }
+                console.log(`Rider transfers from ${startLine} Line to ${endLine} Line at Park Street.`)
+                
+                if ( fromParkStreet > endingPoint){
+                    // From Park Street to the End
+                    stopsAfterChange = fromParkStreet - endingPoint;
+
+                    for (let k = fromParkStreet; k > endingPoint - 1; k--){
+                        stationsAfterChange.push(lines[endLine][k]);   
+                   };
+                    for (let j = 1; j < stationsAfterChange.length - 1; j++){
+                    console.log(`Rider arrives at ${endLine} Line and ${stationsAfterChange[j]}.`)
+                    };
                     
-                }        
-             return `${stops}  stops From ${startStation}  To  ${endStation} `;
+
+                }else{
+                    stopsAfterChange = endingPoint - fromParkStreet;
+                    for (let i = fromParkStreet; i < endingPoint + 1; i++){
+                        stationsAfterChange.push(lines[endLine][i]); 
+                    };
+                     for (let j = 1; j < stationsAfterChange.length - 1; j++){
+                    console.log(`Rider arrives at ${endLine} Line and ${stationsAfterChange[j]}.`)
+                     };
+                    
+                }
+                stops = stopsToChange + stopsAfterChange;
+                console.log(`Rider exits the train at ${endLine} Line and ${endStation}.`);
+            }     
+             
+         return `${stops}  stops From ${startStation}  To  ${endStation} `;
            
     }else{ 
             //if the line name is NOT correct
@@ -71,16 +120,69 @@ const stopsBetweenStations = function(startLine, startStation, endLine, endStati
 }
 
 
-        //==========================RESULTS AFTER TESTING==================================//
+        //==========================RESULTS AFTER TESTING WITH BONUS==================================//
 
-        stopsBetweenStations("Orange", "Chinatown", "Green", "Hynes"); // "7  stops From Chinatown  To  Hynes "
-        stopsBetweenStations("Red", "Central", "Green", "Hynes"); //"6  stops From Central  To  Hynes "
+        stopsBetweenStations("Orange", "Chinatown", "Green", "Hynes"); 
 
-        //testing going back and forth--
-        stopsBetweenStations("Red", "Alewife", "Green", "Kenmore"); //"11  stops From Alewife  To  Kenmore "
-        stopsBetweenStations( "Green", "Kenmore", "Red", "Alewife"); //"11  stops From Kenmore  To  Alewife "
+                    //  Rider boards the train a Orange Line and Chinatown.
+                    //  Rider arrives at Orange Line and Downtown Crossing.
+                    //  Rider arrives at Orange Line and State.
+                    //  Rider transfers from Orange Line to Green Line at Park Street.
+                    //  Rider arrives at Green Line and Boylston.
+                    //  Rider arrives at Green Line and Arlington.
+                    //  Rider arrives at Green Line and Copley.
+                    //  Rider exits the train at Green Line and Hynes.
+                    // "7  stops From Chinatown  To  Hynes "
 
-        //==========================TESTING THE BONUS====================================//
+        stopsBetweenStations("Red", "Central", "Green", "Hynes"); 
+
+                    // Rider boards the train a Red Line and Central.
+                    // Rider arrives at Red Line and Kendall.
+                    // Rider transfers from Red Line to Green Line at Park Street.
+                    // Rider arrives at Green Line and Boylston.
+                    // Rider arrives at Green Line and Arlington.
+                    // Rider arrives at Green Line and Copley.
+                    // Rider exits the train at Green Line and Hynes.
+                    // "6  stops From Central  To  Hynes "
+
+
+        //=============================testing going back and forth====================================//
+       
+        stopsBetweenStations("Red", "Alewife", "Green", "Kenmore"); 
+
+                    // Rider boards the train a Red Line and Alewife.
+                    // Rider arrives at Red Line and Davis.
+                    // Rider arrives at Red Line and Porter.
+                    // Rider arrives at Red Line and Harvard.
+                    // Rider arrives at Red Line and Central.
+                    // Rider arrives at Red Line and Kendall.
+                    // Rider transfers from Red Line to Green Line at Park Street.
+                    // Rider arrives at Green Line and Boylston.
+                    // Rider arrives at Green Line and Arlington.
+                    // Rider arrives at Green Line and Copley.
+                    // Rider arrives at Green Line and Hynes.
+                    // Rider exits the train at Green Line and Kenmore.
+                    // "11  stops From Alewife  To  Kenmore "
+
+        stopsBetweenStations( "Green", "Kenmore", "Red", "Alewife");  
+
+                    // Rider boards the train a Green Line and Kenmore.
+                    // Rider arrives at Green Line and Hynes.
+                    // Rider arrives at Green Line and Copley.
+                    // Rider arrives at Green Line and Arlington.
+                    // Rider arrives at Green Line and Boylston.
+                    // Rider transfers from Green Line to Red Line at Park Street.
+                    // Rider arrives at Red Line and Kendall.
+                    // Rider arrives at Red Line and Central.
+                    // Rider arrives at Red Line and Harvard.
+                    // Rider arrives at Red Line and Porter.
+                    // Rider arrives at Red Line and Davis.
+                    // Rider exits the train at Red Line and Alewife.
+                    // "11  stops From Kenmore  To  Alewife "
+
+
+
+        //==========================TESTING  WRONG INPUT====================================//
 
         // check if the starting station on the correct line! Or there is a mistake in the station name.
         stopsBetweenStations("Orange", "Alewife", "Green", "Hynes"); // " Alewife is NOT on  Orange Line. Please check your departing station!"
@@ -91,7 +193,7 @@ const stopsBetweenStations = function(startLine, startStation, endLine, endStati
 
 
         // check if the rider start and end in the same distnation.
-        stopsBetweenStations("Red", "Park Street", "Green", "Park Street"); //"Hey dude !!  Park Street && Park Street are the Same!!!."
+        stopsBetweenStations("Red", "Park Street", "Green", "Park Street"); //"Hey dude !! Park Street in Red Line is the same as Park Street in Green Line!!!."
 
 
         //check if the name of the starting line is correct.
